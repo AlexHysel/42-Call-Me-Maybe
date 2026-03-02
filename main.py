ec8a0e44f3e84a8f78b422f4b7de129978f0d1ab
AlexHysel <alexhysel@gmail.com>
2026-03-02 20:08:00 +0800

    fixed special chars in string args

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.llm_sdk import Small_LLM_Model
 import json
 import numpy as np
 
+
 class CallMyMaybe:
     def __init__(self) -> None:
         self.llm = Small_LLM_Model()
@@ -52,12 +53,14 @@ class CallMyMaybe:
         return ids
     
     def decode(self, tokens: List[int]):
+        """Translates tokens to text"""
         result = ""
         for token in tokens:
             result += self.vocab[token]
         return result.replace('Ġ', ' ').replace('Ċ', '\n').replace('ĉ', '\t')
     
     def get_logits(self, prompt_ids: list[int], mask = None):
+        """Returns logits for current ids. Adds mask optionally"""
         l = self.llm.get_logits_from_input_ids(self.instruction + prompt_ids)
         if mask is not None:
             l = self.apply_mask(mask, l)
@@ -75,41 +78,48 @@ class CallMyMaybe:
             funcs = [f[1:] for f in funcs if f[0] == next_token and len(f) > 1]
         return result
 
-    def add_args(self, func: dict[str: list[str], str: str], prompt_ids: list[int]):
+    def add_args(self, func: dict, prompt_ids: list[int]):
         """Used to add line with arguments"""
-        prompt_ids += self.encode('\n\t"arguments": {')
-        args = func['args_names']
-        for i, arg in enumerate(args):
+        prompt_ids += self.encode('\n\t\t"arguments": {')
+        for i, arg in enumerate(func['args_names']):
             if i != 0:
                 prompt_ids += self.encode(', ')
             prompt_ids += self.encode(f'"{arg}": ')
-            if func['args_types'][arg] == 'str':
+            is_str = func['args_types'][arg] == 'str'
+            if is_str:
                 prompt_ids += self.encode('"')
             flag = True 
             while flag:
                 logits = self.get_logits(prompt_ids)
-                next = self.decode([int(np.argmax(logits))])
-                for i in range(len(next) -1, -1, -1):
-                    next = next.replace('\n', '\\n')
-                    if next[i] in [',', '}', '\\n']:
-                        next = next[0:i]
+                next_id = int(np.argmax(logits))
+                n = self.decode([next_id])
+                
+                if is_str:
+                    if '"' in n:
+                        n = n[:n.find('"') + 1]
                         flag = False
-                prompt_ids += self.encode(next)
+                    prompt_ids += self.encode(n)
+                else:
+                    if any(c in n for c in [',', '}', '\n']):
+                        flag = False
+                    else:
+                        prompt_ids.append(next_id)
         prompt_ids += self.encode('}\n')
         return prompt_ids
 
     def process_func(self, prompt: str):
         """Process single operation"""
-        prompt_ids = self.encode('{\n\t"prompt": "' + prompt + '",\n')
+        prompt = prompt.replace('\n', '\\n')
+        prompt_ids = self.encode('\t{\n\t\t"prompt": "' + prompt + '",\n')
 
-        prompt_ids += self.encode('\t"function": "')
+        prompt_ids += self.encode('\t\t"function": "')
         func = self.get_func(prompt_ids)
         prompt_ids += func
         prompt_ids += self.encode('",')
 
         prompt_ids = self.add_args(self.funcs[self.decode(func)], prompt_ids)
 
-        prompt_ids += self.encode('}')
+        prompt_ids += self.encode('\t}')
 
         return self.decode(prompt_ids)
 
@@ -120,6 +130,12 @@ if __name__ == "__main__":
     with open('input/function_calling_tests.json') as requests:
         prompts = [t['prompt'] for t in json.load(requests)]
     output = open('output/function_calling_results.json', 'w')
-    for p in prompts:
-        output.write(cmm.process_func(p) + ',\n')
+    output.write('[\n')
+    for i, p in enumerate(prompts):
+        if i < len(prompts) - 1:
+            output.write(cmm.process_func(p) + ',\n')
+        else:
+            output.write(cmm.process_func(p) + '\n')
+    output.write(']')
+    output.close()
         
