d3c903525f0adb2781d4b0ec8353a6db04ac2299
AlexHysel <alexhysel@gmail.com>
2026-05-11 15:10:58 +0800

    df

--- src/__main__.py
+++ src/__main__.py
@@ -58,6 +58,6 @@ if __name__ == "__main__":
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e.msg} " +
               f"at line {e.lineno} column {e.colno}")
-    
+
     except Exception as e:
         print(f"An unexpected error occurred: {str(e)}")
